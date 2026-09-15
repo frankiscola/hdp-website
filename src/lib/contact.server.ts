93168c948a851fe78233f4156142325f70322550
frankiscola <matteo.dragoni93@gmail.com>
2026-09-15 12:47:44 +0200

    Resend to account@

--- src/lib/contact.server.ts
+++ src/lib/contact.server.ts
@@ -9,7 +9,7 @@ export type ContactFormPayload = {
   message: string;
 };
 
-const CONTACT_RECIPIENT = "matteo.dragoni@hyperloopdevelopmentprogram.com";
+const CONTACT_RECIPIENT = "account@hyperloopdevelopmentprogram.com";
 
 function escapeHtml(value: string) {
   return value
