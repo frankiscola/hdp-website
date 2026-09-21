8ae1d00828ef32ada382180b39face7d692968c1
frankiscola <matteo.dragoni93@gmail.com>
2026-09-21 10:29:41 +0200

    Documents final update.

--- src/data/documents.ts
+++ src/data/documents.ts
@@ -66,134 +66,139 @@ export const downloadDocuments: DocumentLink[] = [
 ];
 
 export const resultDocuments: DocumentLink[] = [
+  // 2024
   {
-    title: "HDP Progress report 2021-H1",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-Reporting-2021-H1.pdf",
-    year: "2021",
-  },
-  {
-    title: "HDP Progress report 2021-H2",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-Reporting-2021-H2.pdf",
-    year: "2021",
-  },
-  {
-    title: "HDP Progress report 2022-H1",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-Reporting-2022-H1.pdf",
-    year: "2022",
-  },
-  {
-    title: "HDP Progress report 2022-H2",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-Reporting-2022-H2.pdf",
-    year: "2022",
-  },
-  {
-    title: "HDP Progress report 2023-H1",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-Reporting-2023-H1.pdf",
-    year: "2023",
-  },
-  {
-    title: "HDP Progress report 2023-H2",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-Reporting-2023-H2.pdf",
-    year: "2023",
+    title: "HDP Progress report 2024-H2",
+    href: "/documents/HDP-Reporting-2024-H2.pdf",
+    year: "2024",
   },
   {
     title: "HDP Progress report 2024-H1",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-Reporting-2024-H1.pdf",
+    href: "/documents/HDP-Reporting-2024-H1.pdf",
     year: "2024",
   },
+  // 2023
   {
-    title: "HDP Progress report 2024-H2",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-Reporting-2024-H2.pdf",
-    year: "2024",
+    title: "HDP Progress report 2023-H2",
+    href: "/documents/HDP-Reporting-2023-H2.pdf",
+    year: "2023",
   },
   {
     title: "Impact of hyperloop on Dutch spatial planning towards 2050 (NL)",
-    href: "https://hyperloopdevelopmentprogram.com/download-file/hdp-spatialplanning-nl",
+    href: "/documents/impact-of-hyperloop-on-dutch-spatial-planning-towards-2050-nl.pdf",
     year: "2023",
   },
   {
     title: "Perspectives for hyperloop development in the Netherlands",
-    href: "https://hyperloopdevelopmentprogram.com/download-file/download1406-1",
+    href: "/documents/perspectives-for-hyperloop-development-in-the-netherlands.pdf",
     year: "2023",
   },
   {
+    title: "HDP Progress report 2023-H1",
+    href: "/documents/HDP-Reporting-2023-H1.pdf",
+    year: "2023",
+  },
+  // 2022
+  {
+    title: "HDP Progress report 2022-H2",
+    href: "/documents/HDP-Reporting-2022-H2.pdf",
+    year: "2022",
+  },
+  {
     title: "Hyperconnected Europe Report",
-    href: "https://hyperloopdevelopmentprogram.com/download-file/download1580",
+    href: "/documents/hyperconnected-europe-report.pdf",
     year: "2022",
   },
   {
     title: "Hyperloop handbook for public and private stakeholders",
-    href: "https://hyperloopdevelopmentprogram.com/download-file/download1618",
+    href: "/documents/hyperloop-handbook-for-public-and-private-stakeholders.pdf",
     year: "2022",
   },
   {
     title: "Hyperloop certification roadmap – executive summary",
-    href: "https://hyperloopdevelopmentprogram.com/download-file/download1472",
+    href: "/documents/hyperloop-certification-roadmap-executive-summary.pdf",
     year: "2022",
   },
   {
+    title: "HDP Progress report 2022-H1",
+    href: "/documents/HDP-Reporting-2022-H1.pdf",
+    year: "2022",
+  },
+  // 2021
+  {
+    title: "HDP Progress report 2021-H2",
+    href: "/documents/HDP-Reporting-2021-H2.pdf",
+    year: "2021",
+  },
+  {
     title: "Current state of hyperloop in the Netherlands",
-    href: "https://hyperloopdevelopmentprogram.com/download-file/download1406",
+    href: "/documents/current-state-of-hyperloop-in-the-netherlands.pdf",
     year: "2021",
   },
   {
     title: "Cargo Hyperloop Holland pre-feasibility study",
-    href: "https://hyperloopdevelopmentprogram.com/download-file/download1350",
+    href: "/documents/cargo-hyperloop-holland-pre-feasibility-study.pdf",
     year: "2021",
   },
   {
     title: "Safety Vision – Cargo",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/2021-04-02-Safety-Vision.pdf",
+    href: "/documents/2021-04-02-Safety-Vision.pdf",
     year: "2021",
   },
   {
     title: "Safety Concept – Cargo",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/SafetyConcept_Deliverable_20210603.pdf",
+    href: "/documents/SafetyConcept_Deliverable_20210603.pdf",
     year: "2021",
   },
   {
     title: "Operation Concept Description – Cargo",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/CargoLoop-Operation-Concept-Description-HDP.pdf",
+    href: "/documents/CargoLoop-Operation-Concept-Description-HDP.pdf",
     year: "2021",
   },
   {
     title: "Business Opportunity Definition – Passengers",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-WG-Pax-27-Business-Opportunity-Definition-HDP.pdf",
+    href: "/documents/HDP-WG-Pax-27-Business-Opportunity-Definition-HDP.pdf",
     year: "2021",
   },
   {
     title: "Hyperloop Interior Design Guidelines – Passengers",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/HDP-WG-Pax-114-ADSE-no-21-RA-012-Hyperloop-Interior-Design-Guidelines-v10.pdf",
+    href: "/documents/HDP-WG-Pax-114-ADSE-no-21-RA-012-Hyperloop-Interior-Design-Guidelines-v10.pdf",
     year: "2021",
   },
   {
     title: "User Perceptions of Hyperloop for Passengers",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/Report-User-Perceptions-of-Hyperloop-Thesis-O-Callahan.pdf",
+    href: "/documents/Report-User-Perceptions-of-Hyperloop-Thesis-O-Callahan.pdf",
     year: "2021",
   },
   {
     title: "Future hyperloop research infrastructure",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/Future-hyperloop-research-infrastructure.pdf",
+    href: "/documents/Future-hyperloop-research-infrastructure.pdf",
     year: "2021",
   },
   {
+    title: "HDP Progress report 2021-H1",
+    href: "/documents/HDP-Reporting-2021-H1.pdf",
+    year: "2021",
+  },
+  // 2020
+  {
     title: "Preliminary Route Study 1 – Kuala Lumpur / Singapore",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/Preliminary%20Route%20Study%201%20-%20Kuala%20Lumpur%20Singapore.pdf",
+    href: "/documents/Preliminary-Route-Study-1-Kuala-Lumpur-Singapore.pdf",
     year: "2020",
   },
   {
     title: "Preliminary Route Study 2 – Delhi / Ahmedabad",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/Preliminary%20Route%20Study%202%20-%20Delhi%20-%20Ahmedabad.pdf",
+    href: "/documents/Preliminary-Route-Study-2-Delhi-Ahmedabad.pdf",
     year: "2020",
   },
   {
     title: "Preliminary Route Study 3 – Hannover / Bielefeld",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/Preliminary%20Route%20Study%203%20-%20Hannover%20-%20Bielefeld.pdf",
+    href: "/documents/Preliminary-Route-Study-3-Hannover-Bielefeld.pdf",
     year: "2020",
   },
   {
     title: "Preliminary Route Study 4 – Brisbane / Gold Coast",
-    href: "https://www.hyperloopdevelopmentprogram.com/s/Preliminary%20Route%20Study%204%20-%20Brisbane%20-%20Gold%20Coast.pdf",
+    href: "/documents/Preliminary-Route-Study-4-Brisbane-Gold-Coast.pdf",
     year: "2020",
   },
 ];
